3056a8c0597aec7f88e63dc08880b89e86246529
nedemenang <nnamso.edemenang@gmail.com>
2019-03-29 00:24:23 +0100

    initial project files

--- app/blueprints/base_blueprint.py
+++ app/blueprints/base_blueprint.py
@@ -13,17 +13,6 @@ class BaseBlueprint:
 
         '''Register all App blueprints here '''
         from app.blueprints.sample_blueprint import sample_blueprint
-        from app.blueprints.user_blueprint import user_blueprint
-        from app.blueprints.card_blueprint import card_blueprint
-        from app.blueprints.flight_blueprint import flight_blueprint
-        from app.blueprints.location_blueprint import location_blueprint
-        from app.blueprints.permission_blueprint import permission_blueprint
-        from app.blueprints.ticket_blueprint import ticket_blueprint
-
+        
         self.app.register_blueprint(sample_blueprint)
-        self.app.register_blueprint(user_blueprint)
-        self.app.register_blueprint(card_blueprint)
-        self.app.register_blueprint(flight_blueprint)
-        self.app.register_blueprint(location_blueprint)
-        self.app.register_blueprint(permission_blueprint)
-        self.app.register_blueprint(ticket_blueprint)
+        
